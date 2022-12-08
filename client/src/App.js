7ba1e7616c7906c46ac6f123f60627748e91c0e7
paula <acostapaula7223@gmail.com>
2022-12-07 21:38:22 -0500

    crando ruta dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="App">
         <Switch>
           <Route exact path="/" component={LandingPage} />
+          <Route path="/dashboard" component={Dashboard} />
         </Switch>
       </div>
     </BrowserRouter>
