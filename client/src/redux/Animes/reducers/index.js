8ed7c5269eba7916ddd89ce63c256f5304f4afa3
Your Name <you@example.com>
2022-12-11 02:10:47 -0500

    add component AnimeDetails

--- client/src/redux/Animes/reducers/index.js
+++ client/src/redux/Animes/reducers/index.js
@@ -1,8 +1,10 @@
-import { GET_ANIMES, SEARCH_ANIMES } from "../types";
+import { GET_ANIMES, GET_ANIME_BY_ID, GET_ANIME_EPISODES, SEARCH_ANIMES } from "../types";
 
 const initialState = {
   animes: [],
   anime: [],
+  animeDetails: [],
+  animeEpisodes:[]
 };
 
 function rootReducer(state = initialState, action) {
@@ -18,6 +20,16 @@ function rootReducer(state = initialState, action) {
         ...state,
         anime: action.payload.data,
       };
+    case GET_ANIME_BY_ID:
+      return {
+        ...state,
+        animeDetails: action.payload
+      }
+    case GET_ANIME_EPISODES: 
+      return {
+        ...state,
+        animeEpisodes: action.payload
+      }
     default:
       return state;
   }
