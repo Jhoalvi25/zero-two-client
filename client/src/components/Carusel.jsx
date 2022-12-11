dee64b7d9469256c7a6bee894e666f50f750c5ca
Jhoalvi pereira <101520054+Jhoalvi25@users.noreply.github.com>
2022-12-11 14:42:20 -0500

    estilos terminados

--- client/src/components/Carusel.jsx
+++ client/src/components/Carusel.jsx
@@ -1,27 +1,54 @@
 import React from "react";
-import "../style/Carusel.css"
+import "../style/Carusel.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import caruselImg from "../Carusel-logyc/caruselImg";
 
-
 export default function Carusel() {
+  const [currentImage, setImageCurrent] = React.useState(0);
+
+  const amount = caruselImg?.length;
+
+  if (!Array.isArray(caruselImg) || amount === 0) return;
+
+  const nextImage = () => {
+    setImageCurrent(currentImage === 0 ? amount - 1 : currentImage - 1);
+  };
+
+  const backImage = () => {
+    setImageCurrent(currentImage === amount - 1 ? 0 : currentImage + 1);
+  };
 
   return (
     <div className="center">
       <div className="carusel_cont">
-        <button className="btn" >
+        <button onClick={backImage} className="btn">
           {" "}
           <FontAwesomeIcon icon={faChevronLeft} />
         </button>
 
         <div className="img_carusel_cont fade">
-          <h2>{caruselImg[0].name}</h2>
-          <img src={caruselImg[0].image} width="100%" alt="img" />
+          {caruselImg.map((elem, index) => {
+            return (
+              <div>
+                {currentImage === index && <h2 key={index}>{elem.name}</h2>}
+              </div>
+            );
+          })}
+
+          {caruselImg.map((elem, index) => {
+            return (
+              <div>
+                {currentImage === index && (
+                  <img key={index} src={elem.image}  alt="img" />
+                )}
+              </div>
+            );
+          })}
         </div>
 
-        <button className="btn">
+        <button onClick={nextImage} className="btn">
           {" "}
           <FontAwesomeIcon icon={faChevronRight} />{" "}
         </button>
